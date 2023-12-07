484c39170f1d0980df1a8369ec19b0b9863eb738
Mikhail Filin <mikhailf.codes@gmail.com>
2023-12-07 15:25:26 +0400

    fix: fix bug with timer starting not from the first values

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,12 +1,13 @@
 import { fromEvent, interval, skipUntil } from 'rxjs';
 import { pauseButton, setCount, startButton } from './utilities';
-import { takeUntil } from 'rxjs/operators';
+import { scan, takeUntil } from 'rxjs/operators';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
 const interval$ = interval(1000).pipe(
   skipUntil(start$),
   takeUntil(pause$),
+  scan(acc => acc + 1, 0)
 );
 
 interval$.subscribe(setCount);
