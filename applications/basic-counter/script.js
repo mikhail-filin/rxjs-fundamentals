c344ccd0f9eb48493d2e1fa425683782d4c687aa
Mikhail Filin <mikhailf.codes@gmail.com>
2023-12-07 15:20:40 +0400

    refactor: clean up Combining Operators task

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,12 +1,12 @@
-import { fromEvent, interval, merge, NEVER, skipUntil } from 'rxjs';
-import { setCount, startButton, pauseButton, getCount } from './utilities';
-import { scan, takeUntil } from 'rxjs/operators';
+import { fromEvent, interval, skipUntil } from 'rxjs';
+import { pauseButton, setCount, startButton } from './utilities';
+import { takeUntil } from 'rxjs/operators';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
 const interval$ = interval(1000).pipe(
   skipUntil(start$),
-  takeUntil(pause$)
-).subscribe((value) => {
-  setCount(value);
-});
+  takeUntil(pause$),
+);
+
+interval$.subscribe(setCount);
