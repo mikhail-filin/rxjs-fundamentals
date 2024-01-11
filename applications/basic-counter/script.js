d09a9c27d9747d4c1f726c267e6a31036866822a
Mikhail Filin <mikhailf.codes@gmail.com>
2024-01-11 11:49:02 +0400

    feat: switchMap

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,13 +1,16 @@
-import { fromEvent, interval, skipUntil } from 'rxjs';
+import { fromEvent, interval, merge, NEVER, startWith, switchMap } from 'rxjs';
 import { pauseButton, setCount, startButton } from './utilities';
-import { scan, takeUntil } from 'rxjs/operators';
+import { mapTo, scan } from 'rxjs/operators';
 
-const start$ = fromEvent(startButton, 'click');
-const pause$ = fromEvent(pauseButton, 'click');
-const interval$ = interval(1000).pipe(
-  skipUntil(start$),
-  takeUntil(pause$),
-  scan(acc => acc + 1, 0)
-);
+const start$ = fromEvent(startButton, 'click')
+  .pipe(mapTo(true));
+const pause$ = fromEvent(pauseButton, 'click')
+  .pipe(mapTo(false));
+const interval$ = merge(start$, pause$).pipe(
+  switchMap((isRunning) => {
+    return isRunning ? interval(1000) : NEVER;
+  }),
+  scan((acc) => acc + 1, 0)
+)
 
 interval$.subscribe(setCount);
